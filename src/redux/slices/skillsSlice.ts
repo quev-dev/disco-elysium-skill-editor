f61e3ae9fe078bee9715faa632fa46683b2d442a
QuevDev <quevdev@outlook.com>
2023-07-25 18:11:52 -0400

    Initialized skillsSlice

---
+++ src/redux/slices/skillsSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+// TODO - separate these into sections by attributes
+interface SkillState {
+  logic: number;
+  encyclopedia: number;
+  rhetoric: number;
+  drama: number;
+  conceptualization: number;
+  visualCalculus: number;
+  volition: number;
+  inlandEmpire: number;
+  empathy: number;
+  authority: number;
+  espritDeCorps: number;
+  suggestion: number;
+  endurance: number;
+  painThreshold: number;
+  physicalInstrument: number;
+  electroChemistry: number;
+  shivers: number;
+  halfLight: number;
+  handEyeCoordination: number;
+  perception: number;
+  reactionSpeed: number;
+  savoirFaire: number;
+  interfacing: number;
+  composure: number;
+}

27959d121e47953f1e5e8069c4e1ab0431d87ece
QuevDev <quevdev@outlook.com>
2023-07-26 09:45:26 -0400

    Added diamond svgs

---
+++ src/components/Attribute.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Attribute() {
+  return (
+    <>
+      <p></p>
+    </>
+  );
+}
---
+++ src/components/Skill.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Skill() {
+  return (
+    <>
+      <p></p>
+    </>
+  );
+}

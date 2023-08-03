83578867ac473a03d2dd80b6864d662fc453b428
QuevDev <quevdev@outlook.com>
2023-08-03 12:14:29 -0400

    Added thumbnail & favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,23 @@ import '../styles/globals.css';
 import '../styles/animations.css';
 import '../styles/resize.css';
 import type { Metadata } from 'next';
+// import { NextSeo } from 'next-seo';
 
 export const metadata: Metadata = {
   title: 'Disco Elysium Skill Editor',
-  description:
-    'An unofficial, fan-made site which mimics the attributes and skills page from Disco Elysium.',
+  description: 'A fan-made app mimicking the skills page from Disco Elysium.',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang='en'>
+      <head>
+        <meta name='theme-color' content='#d7441a' />
+      </head>
       <body className='md:text-xl'>{children}</body>
     </html>
   );

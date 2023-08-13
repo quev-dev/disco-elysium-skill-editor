2938629f968519b428c0074deb0b06d9da3314af
QuevDev <quevdev@outlook.com>
2023-08-13 17:41:19 -0400

    Added embed thumbnail to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,22 +2,27 @@ import '../styles/globals.css';
 import '../styles/animations.css';
 import '../styles/resize.css';
 import type { Metadata } from 'next';
-// import { NextSeo } from 'next-seo';
 
 export const metadata: Metadata = {
   title: 'Disco Elysium Skill Editor',
-  description: 'A fan-made app mimicking the skills page from Disco Elysium.',
+  description:
+    'An unofficial, fan-made site which mimics the attributes and skills page from Disco Elysium.',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
       <head>
         <meta name='theme-color' content='#d7441a' />
+        <meta property='og:image:type' content='image/png' />
+        <meta property='og:image:width' content='450' />
+        <meta property='og:image:height' content='400' />
+        <meta property='og:image' content='https://i.imgur.com/rjH9UI7.png' />
+        <meta name='twitter:card' content='summary' />
+        <meta name='twitter:image:type' content='image/png' />
+        <meta name='twitter:image:width' content='450' />
+        <meta name='twitter:image:height' content='400' />
+        <meta name='twitter:image' content='https://i.imgur.com/rjH9UI7.png' />
       </head>
       <body className='md:text-xl'>{children}</body>
     </html>
